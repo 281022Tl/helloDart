1b53855dc5b0925ade50b723795c3e7b7709ad61
281022Tl <428832129@qq.com>
2022-04-11 13:51:05 +0000

    新增dart语法

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -13,45 +13,42 @@ void variables() {
       'name is $name, year is $year, antennaDiameter is $antennaDiameter, flybyObjects is $flybyObjects, image is $image');
 }
 
-//dart语言变量语法
-void test(){
-  late String description; //惰性初始化变量
-  description = 'Feijoada!';
-  print(description);
+// 控制流程 (https://dart.cn/samples#control-flow-statements)
+void control_flow_statements() {
+  print('\n');
+  print('#' * 40);
+  print('控制流程');
+  print('#' * 40);
 
-  final String nickname = 'Bobby'; //不可修改
-  const bar = 1000000; //整型常量
-  print('$nickname, $bar');
+  var year = DateTime.now().year;
 
-  // String -> int
-  var one = int.parse('1'); //字符串与数字之间的转化
-  assert(one == 1);
-  // int -> String
-  String oneAsString = 1.toString();
-  assert(oneAsString == '1');
-  // String -> double
-  var onePointOne = double.parse('1.1');
-  assert(onePointOne == 1.1);
-  // double -> String
-  String piAsString = 3.14159.toStringAsFixed(2);
-  assert(piAsString == '3.14');
-  print('$one,$oneAsString,$onePointOne,$piAsString');
+  if (year >= 2001) {
+    print('21s centry');
+  } else if (year >= 1901) {
+    print('20th centry');
+  }
 
-  var s1 = 'Single quotes work well for string literals.';//字符串
-  print('$s1');
-  var s3 = '使用单引号创建字符串时可以使用斜杠来转义那些与单引号冲突的字符串：\'。';
-  print('$s3');
-  var s = '字符串插值';
-  assert('Dart 有$s，使用起来非常方便。' == 'Dart 有字符串插值，使用起来非常方便。');
-  assert('使用${s.substring(3,5)}表达式也非常方便' == '使用插值表达式也非常方便。');
-  print('$s');
+  var flybyObjects = ['a', 'b', 'c'];
+  for (final object in flybyObjects) {
+    print(object);
+  }
 
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  year = 2009;
+  while (year < 2016) {
+    year += 1;
+    print(year);
+  }
 }
 
 void main(List<String> args) {
   // 变量
   variables();
 
-  //测试
-  test();
+  //控制流程
+  control_flow_statements();
+ 
 }
