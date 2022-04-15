b424abfd95e6e8ce3d0431128c6c060b14ea27fc
281022Tl <428832129@qq.com>
2022-04-15 07:58:59 +0000

    新增dart库函数

---
+++ 02_dart_library.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+//数字，集合，字符串等https://dart.cn/guides/libraries/library-tour#dartcore---numbers-collections-strings-and-more
+void dartcore_digital_string() {
+  //控制台打印
+  print('#'*40);
+  print('I drink &tea.');
+
+  // parse()方法将字符串转换为整型或双浮点型对象
+}
